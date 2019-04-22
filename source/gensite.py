622efcef5c7fbf0742eba134ceaf43dcb1fb33dc
Julius Tarng <julius@tarng.com>
2019-04-22 16:12:25 -0500

    Add support for recursive directories in gensite.py

--- source/gensite.py
+++ source/gensite.py
@@ -5,21 +5,29 @@ import yaml
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def main():
-  for f in os.listdir('content'):
-    p = os.path.join('content', f)
+def genFromDirectory(root='content', dest='../'):
+  for f in os.listdir(root):
+    p = os.path.join(root, f)
 
-    # temporary, todo: recursive directory
-    if '.md' not in p:
+    # Recurse if directory
+    if os.path.isdir(p):
+      genFromDirectory(root + '/' + f, dest + f + '/')
+
+    if not p.endswith('.md'):
        continue
 
     with open(p, 'r', encoding='utf-8') as file:
       s = file.read()
 
+      d = {}  # Data for rendering
+
       # parse frontmatter: ---key:value---\n
       res = re.search('^(---)([\s\S]*)(---)\n([\s\S]*$)', s)
       if res:
         meta = yaml.load(res.group(2))
+        for k in meta:
+          d[k] = meta[k]
+
         content = res.group(4)
       else:
         content = s
@@ -27,18 +35,17 @@ def main():
       content = protect_cors_url(content)
       md = misaka.Markdown(misaka.HtmlRenderer())
 
-      d = {
-        'content': md(content)
-      }
+      d['content'] = md(content)
 
-      for k in meta:
-        d[k] = meta[k]
+      if not os.path.exists(dest):
+        os.makedirs(dest)
 
       n = os.path.splitext(f)[0]
-      with open('../'+n+'.html', 'w', encoding='utf-8') as file:
+      with open(dest+n+'.html', 'w', encoding='utf-8') as file:
         env = Environment(
           loader = FileSystemLoader('templates'))
-        tmpl = env.get_template(meta['template']+'.html')
+        tmplId = d['template'] if 'template' in d.keys() else 'default'
+        tmpl = env.get_template(tmplId+'.html')
         file.write(tmpl.render(page=d))
 
 # Protects cross origin (external) URL links
@@ -50,5 +57,7 @@ def protect_cors_url(md):
       l2 = a.replace('{{URL}}', l[2]).replace('{{LABEL}}', l[1])
       md = md.replace(l[0],l2)
     return md
+  else:
+    return md
 
-main()
+genFromDirectory()
